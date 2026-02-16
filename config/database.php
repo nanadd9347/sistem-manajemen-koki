e9568d13b3046ff62b095fe90cd8b5051223fa08
nanadd9347 <mianessa75@gmail.com>
2026-02-16 21:58:41 +0700

    initial commit

---
+++ config/database.php
@@ -0,0 +1,74 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'db_manajemen_koki');  // Ganti sesuai pilihan database
+
+class Database {
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $dbname = DB_NAME;
+    
+    private $conn;
+    private $stmt;
+    
+    public function __construct() {
+        try {
+            $this->conn = new PDO(
+                "mysql:host=$this->host;dbname=$this->dbname",
+                $this->user,
+                $this->pass,
+                [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            die("Koneksi database gagal: " . $e->getMessage());
+        }
+    }
+    
+    public function query($sql) {
+        $this->stmt = $this->conn->prepare($sql);
+    }
+    
+    public function bind($param, $value, $type = null) {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindValue($param, $value, $type);
+    }
+    
+    public function execute() {
+        return $this->stmt->execute();
+    }
+    
+    public function resultSet() {
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    public function single() {
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    
+    public function rowCount() {
+        return $this->stmt->rowCount();
+    }
+    
+    public function lastInsertId() {
+        return $this->conn->lastInsertId();
+    }
+}
